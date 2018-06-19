aedcbdab5a53fc1c6906cec16e468e50b424c678
lets <lets@B113017621L.imd.local>
2018-06-19 16:24:31 -0300

    ajustes da versão inicial

--- questao1.go
+++ questao1.go
@@ -8,44 +8,46 @@ import (
 )
 
 func pinger(c chan string) {
-	shot := (rand.Intn(10))
+	rand.Seed(time.Now().UnixNano())
+	shot := rand.Intn(10)
 	for shot > -1 {
 
 		switch {
-		case (shot < 3):
+		case shot < 3:
 			for i := 0; i<1; i++ {
 				c <- "erro ping"
 			}
-		case (shot >= 3):
+		case shot >= 3:
 			for i := 0;i<1 ; i++ {
 				c <- "ping"
 			}
 		}
 		rand.Seed(time.Now().UnixNano())
-		shot = (rand.Intn(10))
+		shot = rand.Intn(10)
 	}
 }
 
 func ponger(c chan string) {
 	rand.Seed(time.Now().UnixNano())
-	shot := (rand.Intn(10))
+	shot := rand.Intn(10)
 	for shot > -1 {
 		switch {
-		case (shot < 3):
+		case shot < 3:
 			for i := 0;i<1 ; i++ {
 				c <- "erro pong"
 			}
-		case (shot >= 3):
+		case shot >= 3:
 			for i := 0;i<1 ; i++ {
 				c <- "pong"
 			}
 		}
 		rand.Seed(time.Now().UnixNano())
-		shot = (rand.Intn(10))
+		shot = rand.Intn(10)
 	}
 }
 
 func score(c chan string, p int) {
+
 	sc_i := 0
 	sc_o := 0
 	for sc_i < p && sc_o < p{
@@ -68,10 +70,11 @@ func score(c chan string, p int) {
 	fmt.Printf("Ping %v x Pong %v \n", sc_i, sc_o)
 	os.Exit(0)
 	close(c)
+
 }
 
 func main() {
-	var c chan string = make(chan string)
+	var c = make(chan string)
 
 	var points int
 	fmt.Scanln(&points)
